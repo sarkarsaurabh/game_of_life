88fe82158cc8858f62bb25a7bb3e2a7dbf279f57
sarkarsaurabh <saurabh.sarkar@gmail.com>
2018-05-28 21:54:57 -0400

    quit button

---
+++ Assets/quit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class quit : MonoBehaviour {
+
+	public void doExitGame() {
+		// print("quiting");
+     Application.Quit();
+ 	}
+
+
+}
